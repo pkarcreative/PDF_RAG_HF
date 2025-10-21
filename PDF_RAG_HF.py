4e95aecf580d980656e318faa2101704355a729b
pkarcreative <149239431+pkarcreative@users.noreply.github.com>
2025-10-21 12:35:05 +1100

    Update PDF_RAG_HF.py

--- PDF_RAG_HF.py
+++ PDF_RAG_HF.py
@@ -2,13 +2,13 @@ import streamlit as st
 from tqdm import tqdm 
 import pandas as pd
 from pypdf import PdfReader
-#from langchain.docstore.document import Document as LangchainDocument
-from langchain_core.documents import Document as LangchainDocument
-#from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-#from langchain.memory import ConversationBufferMemory
-from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain.docstore.document import Document as LangchainDocument
+#from langchain_core.documents import Document as LangchainDocument
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+#from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.memory import ConversationBufferMemory
+#from langchain_community.chat_message_histories import ChatMessageHistory
+#from langchain.memory import ConversationBufferMemory
 import requests
 import time
 import numpy as np
@@ -201,6 +201,7 @@ if uploaded_file is not None and 'embedding' in st.session_state:
                 else:
                     st.warning("🔑 **Add your API key for unlimited usage!**")
                     
+
 
 
 
